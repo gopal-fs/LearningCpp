763a95428e052eae53d67a116aada787b21b3abb
gopal-fs <gopalpinapathuni.fs@gmail.com>
2026-03-17 21:23:19 +0530

    Post Order Using Iteration

---
+++ postOrder.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode *> st;
+        vector<int> res;
+
+        TreeNode *curr=root;
+        TreeNode *lastVisitedNode=nullptr;
+        while(curr || !st.empty()){
+            while(curr){
+                st.push(curr);
+                curr=curr->left;
+            }
+            TreeNode *peek=st.top();
+            if(peek->right && lastVisitedNode!=peek->right) curr=peek->right;
+            else{
+                res.push_back(peek->val);
+                lastVisitedNode=peek;
+                st.pop();
+            }
+        }
+        return res;
+    }
+};

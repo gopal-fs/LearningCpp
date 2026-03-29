414557c970d01465bd70d77e0a342b10c4d15a3e
gopal-fs <gopalpinapathuni.fs@gmail.com>
2026-03-29 21:53:44 +0530

    invertATree.cpp

---
+++ invertATree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void find(TreeNode *root){
+        if(!root) return ;
+
+        swap(root->left,root->right);
+        find(root->left);
+        find(root->right);
+    }
+    TreeNode* invertTree(TreeNode* root) {
+        find(root);
+        return root;
+    }
+};
